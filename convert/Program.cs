95c13bd3726102f8ab7407e4380b90f4374c747c
mamroure <tinky93winky@gmail.com>
2020-10-19 11:30:08 +0100

    Add files via upload

---
+++ convert/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+
+                Console.WriteLine("no args bitch");
+                System.Environment.Exit(1);
+            }
+            string fileName = args[0];
+            
+            fileName = fileName.Split(@"\")[fileName.Split(@"\").Length -1];
+            fileName = fileName.Split(".")[0];
+
+
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), $"./vids/{fileName}.mp4");
+
+
+            //Console.WriteLine("FFMPEG");
+            string ffmpeg = @".\ffmpeg\ffmpeg.exe";
+            string argument = $"-i {args[0]} {filePath}";
+
+            Process p = new Process();
+            p.StartInfo.FileName = ffmpeg;
+            p.StartInfo.Arguments = argument;
+
+            p.Start();
+            p.WaitForExit();
+            p.Close();
+            
+           
+        }
+    }
+}
